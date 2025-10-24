ae6c737c99d015a210e5bdef36e616d3cc29f192
Tiago Hinterholz <fh.tiago@gmail.com>
2025-10-24 10:43:46 -0300

    Users apps basic configured

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from apps.users.views.auth_view import RegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
 ]
--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
-    path("api/users", include('apps.users.urls')),
+    path("api/users/", include('apps.users.urls')),
     # path("api/movies", include('apps.movies.urls')),
     # path("api/favorites", include('apps.favorites.urls')),
 ]
